4bf7a0eafd2211e82715ab5cd7a574f61886b762
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-09-15 23:45:51 +0200

    Add styling with StyleSheet for container, text, input and weather info

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { API_KEY } from "@env";
 import { useEffect, useState } from "react";
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 interface WeatherData {
   name: string;
@@ -52,3 +52,29 @@ export default function App() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 20,
+    paddingTop: 60,
+  },
+  title: {fontSize: 28, fontWeight: 'bold', marginBottom: 20 },
+  inputContainer: { flexDirection: 'row', marginBottom: 20 },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    width: 200,
+    marginRight: 10,
+    borderRadius: 5,
+  },
+  weatherContainer: {
+    alignItems: 'center',
+  },
+  city: { fontSize: 24, fontWeight: 'bold' },
+  temp: { fontSize: 20, marginTop: 5 },
+  desc: { fontSize: 16, fontStyle: 'italic', marginTop: 5 },
+})
