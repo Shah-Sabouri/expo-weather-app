d491bfd95c4204a6553b0077487146c497b352ad
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-09-15 23:39:44 +0200

    Setup state, interface and fetchWeather function before return

--- App.tsx
+++ App.tsx
@@ -2,24 +2,39 @@ import { API_KEY } from "@env";
 import { useEffect, useState } from "react";
 import { Text, View } from 'react-native';
 
+interface WeatherData {
+  name: string;
+  main: {
+    temp: number;
+  };
+  weather: { description: string }[];
+}
+
 export default function App() {
-  const [weather, setWeather] = useState<any>(null);
+  const [city, setCity] = useState('Stockholm') // by default
+  const [weather, setWeather] = useState<WeatherData | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchWeather = async (cityName: string) => {
+    try {
+      setLoading(true);
+      const res = await fetch (
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      if (!res.ok) throw new Error('City not found');
+      const data: WeatherData = await res.json();
+      setWeather(data);
+    } catch(err) {
+      console.error(err);
+      setWeather(null);
+      alert('Kunde inte hämta väder för den staden.');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchWeather = async () => {
-      try {
-        const city = "Stockholm";
-        const response = await fetch (
-          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-        );
-        const data = await response.json();
-        setWeather(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchWeather();
+    fetchWeather(city);
   }, []);
 
   return (
